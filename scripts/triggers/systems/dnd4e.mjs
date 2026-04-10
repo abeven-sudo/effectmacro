ce7f5d6d9e198f39aed76629889a50e0cd325d09
abeven-sudo <138735119+abeven-sudo@users.noreply.github.com>
2026-04-10 21:03:23 +1000

    DnD 4e development
    Testing
    
    Testing
    
    Testing
    
    Testing
    
    Push
    
    Testing
    
    Test harness
    
    Beginnings
    
    Update module.json
    
    Update module.json
    
    Update package.json
    
    Update module.json
    
    Update _module.mjs
    
    Update dnd4e.mjs
    
    Add files via upload
    
    Add files via upload

---
+++ scripts/triggers/systems/dnd4e.mjs
@@ -0,0 +1,83 @@
+import { TRIGGERS } from "../../triggers.mjs";
+
+export default function init() {
+  debugger;
+
+  if (game.system.id !== "dnd4e") return;
+
+  // Add D&D 4e system triggers - organized by category
+  TRIGGERS.push(
+    {
+      label: "EFFECTMACRO.dnd4e.4eTriggers",
+      options: [
+        "dnd4e.usePower",
+        "dnd4e.equipItem",
+        "dnd4e.unequipItem"
+      ]
+    }
+  );
+
+  Hooks.on("dnd4e.usePower", usePower);
+  Hooks.on("updateItem", equipItem);
+  Hooks.on("updateItem", unequipItem);
+}
+
+/* -------------------------------------------------- */
+
+/**
+ * Execute all effects that affect an actor and contain this trigger.
+ * This method is called on all clients, but filters out those not to execute it.
+ * @param {foundry.documents.Actor} actor   The actor with the effects.
+ * @param {string} hook                     The trigger name.
+ * @param {object} context                  Additional context to pass to the macro.
+ */
+async function _executeAppliedEffects(actor, hook, context = {}) {
+  if (!effectmacro.utils.isExecutor(actor)) return;
+
+  for (const e of actor.appliedEffects.filter((e) => effectmacro.utils.hasMacro(e, hook)))
+    await effectmacro.utils.callMacro(e, hook, context);
+}
+
+/* -------------------------------------------------- */
+
+/**
+ * On dnd4e power usage.
+ * @param {power} item4e    The used power.
+ * @param {data} data      The actor, alias, scene and token.
+ */
+async function usePower(power, data) {
+  debugger;
+//  const actor = data.subject?.item?.actor;
+  const actor = game.actors.get(data.actor);
+  if (!actor) return;
+  return _executeAppliedEffects(actor, "dnd4e.usePower", { power, data });
+}
+
+/* -------------------------------------------------- */
+
+/**
+ * On update of item.
+ * @param {Roll[]} rolls    The damage rolls.
+ * @param {object} data     Roll configuration data.
+ */
+async function equipItem(item, changed, action, id) {
+  debugger;
+  const actor = action.parent;
+  if (!changed.system?.equipped) return;
+  return _executeAppliedEffects(actor, "dnd4e.equipItem", { item, changed, action, id });
+}
+
+
+/* -------------------------------------------------- */
+
+/**
+ * On update of item.
+ * @param {Roll[]} rolls    The damage rolls.
+ * @param {object} data     Roll configuration data.
+ */
+async function unequipItem(item, changed, action, id) {
+  debugger;
+  const actor = action.parent;
+  if (changed.system?.equipped) return;
+  return _executeAppliedEffects(actor, "dnd4e.unequipItem", { item, changed, action, id});
+}
